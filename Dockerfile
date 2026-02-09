8f0ec02b15cf922fc62b17fc02fcaf6a984a3cd9
ProcessLabs <r.i.galeev@gmail.com>
2026-02-09 12:26:29 +0300

    fix2

--- Dockerfile
+++ Dockerfile
@@ -21,6 +21,6 @@ FROM nginx:alpine
 COPY --from=builder /app/dist /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
-EXPOSE 3000
+EXPOSE 8000
 
 CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 8000,
   },
   preview: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 8000,
   },
   plugins: [
     react(),
